da62bbe4a39af56b62a28323f60841e822bab88e
JokerLiu <liuyuelin0428@163.com>
2017-11-08 15:35:27 +0800

    更新

--- src/com/sorm/core/DBManager.java
+++ src/com/sorm/core/DBManager.java
@@ -1,5 +1,15 @@
 package com.sorm.core;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import com.sorm.bean.Configuration;
+
 /**
  * 根据配置信息，维持连接对象的管理
  * 
@@ -7,5 +17,117 @@ package com.sorm.core;
  *
  */
 public class DBManager {
+	private static Configuration conf;
 
+	static { // 在初始化DBManager对象时将配置文件加载Configuration对象
+		Properties pros = new Properties();
+
+		try {
+			pros.load(Thread.currentThread().getContextClassLoader()
+					.getResourceAsStream("db.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		conf = new Configuration();
+		conf.setDriver(pros.getProperty("driver"));
+		conf.setUser(pros.getProperty("user"));
+		conf.setPwd(pros.getProperty("pwd"));
+		conf.setUrl(pros.getProperty("url"));
+		conf.setUsingDB(pros.getProperty("usingDB"));
+		conf.setSrcPath(pros.getProperty("srcPath"));
+		conf.setPoPackage(pros.getProperty("poPackage"));
+	}
+
+	/**
+	 * 获取连接
+	 * 
+	 * @return
+	 */
+	public static Connection getConn() {
+		try {
+			Class.forName(conf.getDriver());
+			return DriverManager.getConnection(conf.getUrl(), conf.getUser(), conf.getPwd()); // 直接建立连接，后期增加连接池处理，提高效率！！！
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * 关闭连接
+	 * 
+	 * @param rs
+	 * @param ps
+	 * @param conn
+	 */
+	public static void close(ResultSet rs, Statement ps, Connection conn) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (ps != null) {
+				ps.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 关闭连接
+	 * 
+	 * @param ps
+	 * @param conn
+	 */
+	public static void close(Statement ps, Connection conn) {
+		try {
+			if (ps != null) {
+				ps.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 关闭连接
+	 * 
+	 * @param conn
+	 */
+	public static void close(Connection conn) {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 返回Configuration对象
+	 * 
+	 * @return
+	 */
+	public static Configuration getConf() {
+		return conf;
+	}
 }
