e55abc6fa17878a37fc85bb663d15dc95ed74ce5
JokerLiu <liuyuelin0428@163.com>
2017-11-08 14:40:12 +0800

    新增工具类

---
+++ src/com/sorm/utils/JavaFileUtils.java
@@ -0,0 +1,11 @@
+package com.sorm.utils;
+
+/**
+ * 封装采用的Java文件操作
+ * 
+ * @author Joker
+ *
+ */
+public class JavaFileUtils {
+
+}
---
+++ src/com/sorm/utils/ReflectUtils.java
@@ -0,0 +1,11 @@
+package com.sorm.utils;
+
+/**
+ * 封装采用的反射操作
+ * 
+ * @author Joker
+ *
+ */
+public class ReflectUtils {
+
+}
